def286002c579771af3ff2e4ab955709718bc2eb
antonmontrezor <montrezoranton991@gmail.com>
2023-04-11 17:40:46 -0400

    finish Avoid implementation details

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -3,19 +3,23 @@
 
 import * as React from 'react'
 // 🐨 add `screen` to the import here:
-import {render, fireEvent} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Counter from '../../components/counter'
 
-test('counter increments and decrements when the buttons are clicked', () => {
-  const {container} = render(<Counter />)
-  // 🐨 replace these with screen queries
-  // 💰 you can use `getByText` for each of these (`getByRole` can work for the button too)
-  const [decrement, increment] = container.querySelectorAll('button')
-  const message = container.firstChild.querySelector('div')
+test('counter increments and decrements when the buttons are clicked', async () => {
+  // in new version we need to setup the userEvent
+  const user = userEvent.setup()
+  render(<Counter />)
+  const increment = screen.getByRole('button', {name: /increment/i})
+  const decrement = screen.getByRole('button', {name: /decrement/i})
+  const message = screen.getByText(/current count/i)
 
   expect(message).toHaveTextContent('Current count: 0')
-  fireEvent.click(increment)
+  // we should prefer using userEvent over fireEvent as if the event in implementattion changes from onClick to mouseUp, it won't break our tests
+  await user.click(increment)
   expect(message).toHaveTextContent('Current count: 1')
-  fireEvent.click(decrement)
+  // in new version of userEvent, it's methods return promises that we have to handle
+  await user.click(decrement)
   expect(message).toHaveTextContent('Current count: 0')
 })
