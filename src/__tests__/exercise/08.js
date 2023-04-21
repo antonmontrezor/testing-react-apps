cfa06d473cbdceb975672c5328f1329cdfdd8506
antonmontrezor <montrezoranton991@gmail.com>
2023-04-20 22:19:11 -0400

    complete "testing custom hooks"

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,51 +2,108 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import {render, act, renderHook} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-function TestedComponent() {
-  const {count, increment, decrement} = useCounter()
-  return (
-    <div>
-      <div>Current count: {count}</div>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={increment}>Increment</button>
-    </div>
-  )
+// (...args) because we can call setup with multiple arguments
+function setup(...args) {
+  const returnVal = {}
+  function TestComponent() {
+    Object.assign(returnVal, useCounter(...args))
+    return null
+  }
+
+  render(<TestComponent />)
+  return returnVal
 }
 
 // this test has a good coverage of the useCounter functionality, but we could also add a test for the config like initialCount, step (check the hook)
-test('exposes the count and increment/decrement functions', async () => {
-  const user = userEvent.setup()
-  render(<TestedComponent />)
+test('exposes the count and increment/decrement functions', () => {
+  // we cannot destructure as we need to keep the binding to what useCounter returns like count, increment, decrement. we are just updating properties on the same object
+  const returnVal = setup()
 
-  const message = screen.getByText(/current count/i)
-  const increment = screen.getByRole('button', {name: /increment/i})
-  const decrement = screen.getByRole('button', {name: /decrement/i})
+  expect(returnVal.count).toBe(0)
+  act(() => {
+    returnVal.increment()
+  })
+  expect(returnVal.count).toBe(1)
+  act(() => {
+    returnVal.decrement()
+  })
+  expect(returnVal.count).toBe(0)
+})
 
-  expect(message).toHaveTextContent('Current count: 0')
-  await user.click(increment)
-  expect(message).toHaveTextContent('Current count: 1')
-  await user.click(decrement)
-  expect(message).toHaveTextContent('Current count: 0')
+test('allows customization of the initial count', async () => {
+  const returnVal = setup({initialCount: 2})
+
+  expect(returnVal.count).toBe(2)
+  act(() => {
+    returnVal.increment()
+  })
+  expect(returnVal.count).toBe(3)
+  act(() => {
+    returnVal.decrement()
+  })
+  expect(returnVal.count).toBe(2)
+})
+
+test('allows customization of the step', async () => {
+  const returnVal = setup({step: 2})
+
+  expect(returnVal.count).toBe(0)
+  act(() => {
+    returnVal.increment()
+  })
+  expect(returnVal.count).toBe(2)
+  act(() => {
+    returnVal.decrement()
+  })
+  expect(returnVal.count).toBe(0)
 })
 
 test('exposes the count and increment/decrement functions with custom initial count and steps', async () => {
-  const user = userEvent.setup()
-  render(<TestedComponent />)
-  screen.debug()
+  const {result} = renderHook(() => useCounter({initialCount: 2, step: 2}))
 
-  const message = screen.getByText(/current count/i)
-  const increment = screen.getByRole('button', {name: /increment/i})
-  const decrement = screen.getByRole('button', {name: /decrement/i})
-
-  expect(message).toHaveTextContent('Current count: 0')
-  await user.click(increment)
-  expect(message).toHaveTextContent('Current count: 1')
-  await user.click(decrement)
-  expect(message).toHaveTextContent('Current count: 0')
+  expect(result.current.count).toBe(2)
+  act(() => {
+    result.current.increment()
+  })
+  expect(result.current.count).toBe(4)
+  act(() => {
+    result.current.decrement()
+  })
+  expect(result.current.count).toBe(2)
 })
 
+// the same test as the 1st one but with renderHook
+test('exposes the count and increment/decrement functions', () => {
+  const {result} = renderHook(useCounter)
+
+  expect(result.current.count).toBe(0)
+  act(() => {
+    result.current.increment()
+  })
+  expect(result.current.count).toBe(1)
+  act(() => {
+    result.current.decrement()
+  })
+  expect(result.current.count).toBe(0)
+})
+
+test('step can change', () => {
+  const {result, rerender} = renderHook(useCounter)
+
+  expect(result.current.count).toBe(0)
+  act(() => {
+    result.current.increment()
+  })
+  expect(result.current.count).toBe(1)
+
+  rerender({step: 5})
+  
+  act(() => {
+    result.current.decrement()
+  })
+  expect(result.current.count).toBe(-4)
+})
 /* eslint no-unused-vars:0 */
